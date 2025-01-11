20540d5d80a78fc9e296348bb64f6efe9daed832
Ali Mohammed <104313196+engali1986@users.noreply.github.com>
2025-01-11 14:06:38 +0200

    Update LanguageToggle.js

--- src/components/LanguageToggle.js
+++ src/components/LanguageToggle.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 import i18n from '../i18n/i18n.js';
 
 export default function LanguageToggle() {
+  const [language, setLanguage] = useState(i18n.locale); // State to track current language
+
   const toggleLanguage = () => {
     const newLanguage = i18n.locale === 'en' ? 'ar' : 'en';
     i18n.locale = newLanguage;
+    setLanguage(newLanguage); // Update state to trigger re-render
   };
 
   return (
